c187a90ea3342e0594a2c1fd4d724b6af0e39036
Christopher Wattaul <chris.wattaul@gmail.com>
2020-05-14 15:43:29 +0200

    Update MemeGenerator.js

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,40 +1,98 @@
-import react, { useState } from 'react';
+import react, { useState, useEffect } from 'react';
 // this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
+import axios from 'axios';
 
-const memeLink =
-  'https://memegen.link/{image}/{topText}/{bottomText}.jpg?watermark=none';
+const flexStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: space-between;
+  text-align: center;
+`;
 
-export default function CreateMeme(props) {
-  const [image, setImage] = useState([]);
-  const [topText, setTopText] = useState('');
-  const [bottomText, setBottomText] = useState('');
-  const [activeImage, setActiveImage] = useState('');
+const imageStyle = css`
+  height: 1fr;
+  width: 1fr;
+`;
+
+export default function CreateMeme() {
+  const [image, setImage] = useState('bihw');
+  const [topText, setTopText] = useState('it aint much');
+  const [bottomText, setBottomText] = useState('but its honest work');
+  const [data, setData] = useState([]);
+  const [select, setSelect] = useState([]);
+  const [newUrl, setNewUrl] = useState(
+    'https://memegen.link/' +
+      image +
+      '/' +
+      topText +
+      '/' +
+      bottomText +
+      '/.jpg?watermark=none',
+  );
+
+  useEffect(() => {
+    fetch('https://memegen.link/api/templates/', {
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then(res => res.json())
+      .then(data => {
+        const imgData = Object.keys(data);
+        setSelect(imgData);
+      })
+      .catch(e => console.log(e));
+  }, []);
+  console.log(select);
+
   return (
-    <form>
-      <input
-        type='text'
-        name='image'
-        placeholder='your picture'
-        value={image}
-        onChange={e => setImage(e.target.value)}
-      />
-      <input
-        type='text'
-        name='Line 1'
-        placeholder='upper line'
-        value={topText}
-        onChange={e => setTopText(e.target.value)}
-      />
-      <input
-        type='text'
-        name='Line 2'
-        placeholder='bottom line'
-        value={bottomText}
-        onChange={e => setBottomText(e.target.value)}
-      />
-      <button onClick={() => {}}>Submit</button>
-    </form>
+    <div css={flexStyle}>
+      <form>
+        <select>
+          {select.map((name, i) => {
+            return (
+              <option key={i} value={name}>
+                {name}
+              </option>
+            );
+          })}
+        </select>
+
+        <input
+          type='text'
+          name='Line 1'
+          placeholder='upper line'
+          value={topText}
+          onChange={e => setTopText(e.target.value)}
+        />
+        <input
+          type='text'
+          name='Line 2'
+          placeholder='bottom line'
+          value={bottomText}
+          onChange={e => setBottomText(e.target.value)}
+        />
+        <button
+          type='button'
+          onClick={() =>
+            setNewUrl(
+              'https://memegen.link/' +
+                image +
+                '/' +
+                topText +
+                '/' +
+                bottomText +
+                '/.jpg?watermark=none',
+            )
+          }
+        >
+          Submit
+        </button>
+      </form>
+
+      <div>
+        <img css={imageStyle} src={newUrl} alt='Your meme here' />
+      </div>
+    </div>
   );
 }
